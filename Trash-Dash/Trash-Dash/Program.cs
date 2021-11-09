2a7d5ecba36a7bc8ca5455d38f483b1cca092fc3
Ryan Garrity <rgarrity7000@gmail.com>
2021-11-09 15:42:03 -0500

    First Commit!

---
+++ Trash-Dash/Trash-Dash/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Trash_Dash
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
